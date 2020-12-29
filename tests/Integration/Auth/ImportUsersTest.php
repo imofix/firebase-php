79fe01aa28031b83fa4565808636ad37eec562a7
Jurica Šeparović <jurica.separovic@gmail.com>
2020-12-29 14:09:04 +0100

    Clean tests

--- tests/Integration/Auth/ImportUsersTest.php
+++ tests/Integration/Auth/ImportUsersTest.php
@@ -32,7 +32,7 @@ class ImportUsersTest extends IntegrationTestCase
                     ->withPhotoUrl($photoUrl = 'https://example.org/photo.jpg')
                     ->withPhoneNumber($phoneNumber = '+1234567'.\random_int(1000, 9999))
                     ->withVerifiedEmail($email = $uid.'@example.org')
-                    ->withCustomClaims(['admin' => true]),
+                    ->withCustomClaims($claims = ['admin' => true]),
             ]
         );
 
@@ -44,7 +44,7 @@ class ImportUsersTest extends IntegrationTestCase
         $this->assertSame($phoneNumber, $user->phoneNumber);
         $this->assertSame($email, $user->email);
         $this->assertTrue($user->emailVerified);
-        $this->assertEquals(['admin' => true], $user->customClaims);
+        $this->assertEquals($claims, $user->customClaims);
         $this->assertFalse($user->disabled);
 
         $this->auth->deleteUser($user->uid);
@@ -55,43 +55,32 @@ class ImportUsersTest extends IntegrationTestCase
         $this->auth->createUser(
             CreateUser::new()
                 ->withUid($uid = \bin2hex(\random_bytes(5)))
-                ->withDisplayName($displayName = 'Old display name')
-                ->withPhotoUrl($photoUrl = 'https://example.org/old-photo.jpg')
-                ->withPhoneNumber($phoneNumber = '+12345674')
                 ->withVerifiedEmail($email = $uid.'@example.org')
-                ->markAsDisabled()
+                ->withPhoneNumber( '+1234567'.\random_int(1000, 9999))
+                ->withPhotoUrl('https://example.org/old-photo.jpg')
+                ->withDisplayName('Old display name')
         );
-
-        $user = $this->auth->getUser($uid);
-
-        $this->assertSame($uid, $user->uid);
-        $this->assertSame($displayName, $user->displayName);
-        $this->assertSame($photoUrl, $user->photoUrl);
-        $this->assertSame($phoneNumber, $user->phoneNumber);
-        $this->assertSame($email, $user->email);
-        $this->assertTrue($user->emailVerified);
-        $this->assertFalse($user->disabled);
 
         $this->auth->importUsers(
             [
                 ImportUserRecord::new()
                     ->withUid($uid)
-                    ->withDisplayName($displayName = 'Some display name')
-                    ->withPhotoUrl($photoUrl = 'https://example.org/photo.jpg')
-                    ->withPhoneNumber($phoneNumber = '+1234567'.\random_int(1000, 9999))
-                    ->withVerifiedEmail($email = $uid.'@example.org')
-                    ->markAsEnabled(),
+                    ->withDisplayName($newDisplayName = 'Some display name')
+                    ->withPhotoUrl($newPhotoUrl = 'https://example.org/photo.jpg')
+                    ->withVerifiedEmail($email)
             ]
         );
 
         $user = $this->auth->getUser($uid);
 
         $this->assertSame($uid, $user->uid);
-        $this->assertSame($displayName, $user->displayName);
-        $this->assertSame($photoUrl, $user->photoUrl);
-        $this->assertSame($phoneNumber, $user->phoneNumber);
+        $this->assertSame($newDisplayName, $user->displayName);
+        $this->assertSame($newPhotoUrl, $user->photoUrl);
         $this->assertSame($email, $user->email);
         $this->assertTrue($user->emailVerified);
+
+        // Values that are not provided with import request fallback to default state
+        $this->assertNull($user->phoneNumber);
         $this->assertFalse($user->disabled);
 
         $this->auth->deleteUser($user->uid);
