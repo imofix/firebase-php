219593f1814576dd677721a6737c4a31c5d25d8b
Ivan Wolf <7150803+ivan-wolf@users.noreply.github.com>
2021-02-10 10:02:44 +0100

    Use REST API for Firestore

---
+++ src/Firebase/Firestore/ApiClient.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreait\Firebase\Firestore;
+
+use GuzzleHttp\ClientInterface;
+use Kreait\Firebase\Exception\FirebaseException;
+use Kreait\Firebase\Exception\FirestoreApiExceptionConverter;
+use Kreait\Firebase\Exception\FirestoreException;
+use Kreait\Firebase\Http\WrappedGuzzleClient;
+use Kreait\Firebase\Util\JSON;
+use Throwable;
+
+/**
+ * @internal
+ */
+class ApiClient implements ClientInterface
+{
+    use WrappedGuzzleClient;
+
+    /** @var FirestoreApiExceptionConverter */
+    private $errorHandler;
+
+    /**
+     * @internal
+     */
+    public function __construct(ClientInterface $client)
+    {
+        $this->client = $client;
+        $this->errorHandler = new FirestoreApiExceptionConverter();
+    }
+
+    /**
+     * @return mixed
+     * @throws FirebaseException
+     * @throws FirestoreException
+     */
+    public function get(string $path, array $options = [])
+    {
+        return $this->requestApi('GET', $path, $options);
+    }
+
+    /**
+     * @return mixed
+     * @throws FirebaseException
+     * @throws FirestoreException
+     */
+    public function patch(string $path, array $data, array $options = [])
+    {
+        $options['json'] = $data;
+
+        return $this->requestApi('PATCH', $path, $options);
+    }
+
+    /**
+     * @throws FirestoreException
+     * @throws FirebaseException
+     * @return mixed
+     */
+    private function requestApi(string $method, string $uri, array $options = [])
+    {
+        try {
+            $response = $this->request($method, $uri, $options);
+
+            return JSON::decode((string) $response->getBody());
+        } catch (Throwable $e) {
+            throw $this->errorHandler->convertException($e);
+        }
+    }
+}
