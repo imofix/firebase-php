6b88b1e3d284b29471d9ec401afd5fb65f8d58d6
Ivan Wolf <7150803+ivan-wolf@users.noreply.github.com>
2021-02-10 10:02:44 +0100

    Use REST API for Firestore

--- src/Firebase/Firestore.php
+++ src/Firebase/Firestore.php
@@ -4,26 +4,26 @@ declare(strict_types=1);
 
 namespace Kreait\Firebase;
 
-use Google\Cloud\Firestore\FirestoreClient;
+use Kreait\Firebase\Firestore\ApiClient;
 
 final class Firestore
 {
-    /** @var FirestoreClient */
+    /** @var ApiClient */
     private $client;
 
     private function __construct()
     {
     }
 
-    public static function withFirestoreClient(FirestoreClient $firestoreClient): self
+    public static function withApiClient(ApiClient $apiClient): self
     {
         $firestore = new self();
-        $firestore->client = $firestoreClient;
+        $firestore->client = $apiClient;
 
         return $firestore;
     }
 
-    public function database(): FirestoreClient
+    public function database(): ApiClient
     {
         return $this->client;
     }
